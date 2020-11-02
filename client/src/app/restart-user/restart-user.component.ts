47e5515ec16dabe8ce36ed22be24a37c47ee1d50
Diego Cortez <diego.fernando1399@gmail.com>
2020-11-01 19:22:14 -0600

    i Restaurar Contraseña HTML

---
+++ client/src/app/restart-user/restart-user.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-restart-user',
+  templateUrl: './restart-user.component.html',
+  styleUrls: ['./restart-user.component.css']
+})
+export class RestartUserComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}

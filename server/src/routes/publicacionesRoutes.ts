b02f5ff1271c52f3a994187dee1f67a6dc5f8e6f
Pablo Fonseca <pablonck@gmail.com>
2020-11-11 23:41:28 -0600

    Rutas para info de publicaciones
    Rutas para información de cada tipo de publicación

--- server/src/routes/publicacionesRoutes.ts
+++ server/src/routes/publicacionesRoutes.ts
@@ -196,6 +196,142 @@ class PublicacionesRoutes{
                 console.log(Exception);
             }
         });
+
+        //******************************************get info publicacion tipo 1************************************************************ */
+        this.router.get('/tipo1', async function (req, res) {
+            try{
+                var cadena = "Select Publicacion.idPublicacion,Publicacion.usuario_carnet,Usuario.nombres,Usuario.apellidos,Publicacion.mensaje,Publicacion.fecha, Publicacion.curso_CodigoCurso, Curso.nombre,Publicacion.tipo FROM ((Publicacion JOIN Usuario ON Publicacion.usuario_carnet = Usuario.carnet) JOIN Curso ON Publicacion.curso_CodigoCurso = Curso.codigoCurso);";
+                var con = new mssql.ConnectionPool(config);
+
+                con.connect(function(err:any){
+                var req = new mssql.Request(con);
+                if(err){
+                    console.log(err);
+                    return;
+                }
+            req.query(cadena,function(err:any, recordset:any){
+                    if(err){
+                        console.log(err);
+                    }
+                    else{
+                        res.send(JSON.stringify(recordset.recordsets[0]));
+                    }
+                    con.close();
+                });
+            });                
+            }catch(Exception){
+                console.log(Exception);
+            }
+        });
+
+        //******************************************get info publicacion tipo 2*********************************************************** */
+        this.router.get('/tipo2', async function (req, res) {
+            try{
+                var cadena = "Select Publicacion.idPublicacion,Publicacion.usuario_carnet,Usuario.nombres,Usuario.apellidos,Publicacion.mensaje,Publicacion.fecha, Publicacion.catedratico_NoCatedratico,Catedratico.nombres,Catedratico.apellidos,Publicacion.tipo FROM ((Publicacion JOIN Usuario ON Publicacion.usuario_carnet = Usuario.carnet) JOIN Catedratico ON Publicacion.catedratico_NoCatedratico = Catedratico.noCatedratico);";
+                var con = new mssql.ConnectionPool(config);
+
+                con.connect(function(err:any){
+                var req = new mssql.Request(con);
+                if(err){
+                    console.log(err);
+                    return;
+                }
+            req.query(cadena,function(err:any, recordset:any){
+                    if(err){
+                        console.log(err);
+                    }
+                    else{
+                        res.send(JSON.stringify(recordset.recordsets[0]));
+                    }
+                    con.close();
+                });
+            });                
+            }catch(Exception){
+                console.log(Exception);
+            }
+        });
+
+        //******************************************get info publicacion tipo 3************************************************************ */
+        this.router.get('/tipo3', async function (req, res) {
+            try{
+                var cadena = "Select Publicacion.idPublicacion,Publicacion.usuario_carnet,Usuario.nombres,Usuario.apellidos,Publicacion.mensaje,Publicacion.fecha, Publicacion.curso_Catedratico_idCatedraticoCurso,Publicacion.curso_CodigoCurso,Curso.codigoCurso,Curso.nombre,Publicacion.catedratico_NoCatedratico,Catedratico.noCatedratico,Catedratico.nombres,Catedratico.apellidos,Publicacion.tipo FROM ((((Publicacion JOIN Curso_Catedratico ON Publicacion.curso_Catedratico_idCatedraticoCurso = Curso_Catedratico.idCatedraticoCurso) JOIN Usuario ON Publicacion.usuario_carnet = Usuario.carnet) JOIN Curso ON Curso.codigoCurso = Curso_Catedratico.curso_CodigoCurso) JOIN Catedratico ON Catedratico.noCatedratico= Curso_Catedratico.catedratico_NoCatedratico);";
+                var con = new mssql.ConnectionPool(config);
+
+                con.connect(function(err:any){
+                var req = new mssql.Request(con);
+                if(err){
+                    console.log(err);
+                    return;
+                }
+            req.query(cadena,function(err:any, recordset:any){
+                    if(err){
+                        console.log(err);
+                    }
+                    else{
+                        res.send(JSON.stringify(recordset.recordsets[0]));
+                    }
+                    con.close();
+                });
+            });                
+            }catch(Exception){
+                console.log(Exception);
+            }
+        });
+
+        //******************************************get info publicacion tipo 4************************************************************ */
+        this.router.get('/tipo4', async function (req, res) {
+            try{
+                var cadena = "SELECT Publicacion.idPublicacion, Publicacion.usuario_carnet, Usuario.nombres, Usuario.apellidos, Publicacion.mensaje,Publicacion.fecha, Publicacion.auxiliar_idAuxiliar,Auxiliar.nombres,Publicacion.tipo FROM ((Publicacion JOIN Usuario ON Publicacion.usuario_carnet = Usuario.carnet) JOIN Auxiliar ON Publicacion.auxiliar_idAuxiliar = Auxiliar.noAuxiliar);";
+                var con = new mssql.ConnectionPool(config);
+
+                con.connect(function(err:any){
+                var req = new mssql.Request(con);
+                if(err){
+                    console.log(err);
+                    return;
+                }
+            req.query(cadena,function(err:any, recordset:any){
+                    if(err){
+                        console.log(err);
+                    }
+                    else{
+                        res.send(JSON.stringify(recordset.recordsets[0]));
+                    }
+                    con.close();
+                });
+            });                
+            }catch(Exception){
+                console.log(Exception);
+            }
+        });
+
+        //******************************************get info publicacion tipo 5************************************************************ */
+        this.router.get('/tipo5', async function (req, res) {
+            try{
+                var cadena = "SELECT Publicacion.idPublicacion,Publicacion.usuario_carnet, Usuario.nombres, Usuario.apellidos, Publicacion.mensaje,Publicacion.fecha, Publicacion.curso_CodigoCurso,Curso.nombre, Publicacion.auxiliar_Curso,Publicacion.auxiliar_idAuxiliar,Auxiliar.nombres,Publicacion.tipo FROM ((((Publicacion JOIN Usuario ON Publicacion.usuario_carnet = Usuario.carnet) JOIN Curso ON Publicacion.curso_CodigoCurso = Curso.codigoCurso) JOIN Curso_Catedratico ON Publicacion.auxiliar_Curso = Curso_Catedratico.idCatedraticoCurso) JOIN Auxiliar ON Publicacion.auxiliar_idAuxiliar = Auxiliar.noAuxiliar);";
+                var con = new mssql.ConnectionPool(config);
+
+                con.connect(function(err:any){
+                var req = new mssql.Request(con);
+                if(err){
+                    console.log(err);
+                    return;
+                }
+            req.query(cadena,function(err:any, recordset:any){
+                    if(err){
+                        console.log(err);
+                    }
+                    else{
+                        res.send(JSON.stringify(recordset.recordsets[0]));
+                    }
+                    con.close();
+                });
+            });                
+            }catch(Exception){
+                console.log(Exception);
+            }
+        });
+
     }
 }
 
